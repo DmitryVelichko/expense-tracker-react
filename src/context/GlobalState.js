f0da64752c09acbe2a1403deb55f458884c05c33
unknown <soleil-2005@mail.ru>
2021-06-22 08:10:14 +0300

    first commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -15,6 +15,11 @@ export const GlobalContext = createContext(initialState);
 
 // Provider component 
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer);
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>{children}</GlobalContext.Provider>
+    );
 } 
 
